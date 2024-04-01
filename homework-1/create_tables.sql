1d0cc247111552bf13413c2b4a0d9c4b4039f8ce
Zotov Stanislav <zotovstanislav@mail.ru>
2024-04-01 14:19:27 +0300

    Добавил sql - команды для создания таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customer
+(
+    customer_id varchar(5) PRIMARY KEY,
+    company_name varchar(100),
+    contact_name varchar(100)
+);
+
+CREATE TABLE employee
+(
+    employee_id int PRIMARY KEY,
+    first_name  varchar(100),
+    last_name  varchar(100),
+	title  varchar(100),
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(5) REFERENCES customer(customer_id) NOT NULL,
+    employee_id int REFERENCES employee(employee_id) NOT NULL,
+	order_date date,
+    ship_city varchar(100)
+);
